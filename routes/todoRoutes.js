6c8b7dc84744afacef555efac9729c446601e2e2
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-07-05 18:19:29 +0530

    routes added

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { createTodo } from '../controllers/todoControllers.js'
+import { verifyToken } from '../connect.js'
 
 const router = express.Router()
 
-router.post('/createTodo', createTodo)
+router.post('/createTodo', verifyToken, createTodo)
 
 export default router
