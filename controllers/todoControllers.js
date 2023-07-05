56274235b04ce036f6e3c0f976972b27f18e6c0d
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-07-05 18:19:11 +0530

    controllers updates

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
+import Users from "../model/Users.js";
+import { ObjectId } from "mongodb";
+export const createTodo = async (req, res, next) => {
+    const newTodo = {
+        title: req.body.title,
+        active: true,
+        id: new ObjectId()
+    }
+    try {
+        const updates = await Users.findByIdAndUpdate(req.user.id, {
+            $push: { todos: newTodo }
+        }, { new: true })
+        res.status(200).json(updates.todos)
+    } catch (error) {
+        next(error)
+    }
+}
 
-export const createTodo = async () => {
+export const createSubTodo = async (req, res, next) => {
+    const newSubTodo = {
+        id: req.body.id,
+        title: req.body.title,
+        active: true
+    }
 
+    const updates = await Users.findOneAndUpdate(req.user.id, {
+        $push:{}
+    })
 }
