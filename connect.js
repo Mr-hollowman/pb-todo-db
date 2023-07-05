3cb9beef06bfd5f8171f4740c28cf740fea5a16d
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-07-05 18:18:47 +0530

    connect function

--- connect.js
+++ connect.js
@@ -1,14 +1,27 @@
+import { MongoClient } from "mongodb";
+import jwt from 'jsonwebtoken';
 import mongoose from "mongoose";
-
 export const connect = async () => {
     mongoose.connect(process.env.MONGO_URI).then(() => {
         console.log("db connected")
     }).catch(err => { throw err })
 }
 
-export const createError = (status, message)=>{
+export const createError = (status, message) => {
     const err = new Error()
     err.status = status
     err.message = message
     return err
+}
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) return next(createError(401, "You are not authendicated!"))
+
+    jwt.verify(token, process.env.JWT, (err, user) => {
+        if (err) return next(createError(403, "Token is not valid"))
+
+        req.user = user
+        next()
+    })
 }
