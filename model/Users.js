a015d93a64aaf7407874fcf90c39bee256157a26
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-07-05 18:19:20 +0530

    user model

--- model/Users.js
+++ model/Users.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
-const UserScema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     img: { type: String, },
+    todos: { type: Array },
+    subTodos: { type: Array }
 }, { timestamps: true })
 
-export default mongoose.model("User", UserScema)
+export default mongoose.model("User", UserSchema)
